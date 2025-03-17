2600ce1c03f9f0a770c05b659fe631acf6e2021b
Jayasree R1 <rangujayashree@gmail.com>
2025-03-17 13:11:01 -0400

    Done Competitive Coding 1

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,17 @@
+// To find the missing number in a sorted array
+//TC is O(logn)
+//SC is O(1).
 
+//Since the array is sorted I explored BS approach. I tried to get the hold of the window in which 
+//the missing element could be based on below condition 
+//"index + some constant = arr[index]"
+//check the above condition at mid and move low,high accordingly
+//once the low,high,mid is tabularised you'll see a pattern in which missed element is always loww+1
+
+//Executed on Linkedin: Yes
+
+
+// "static void main" must be defined in a public class.
+public class Problem1 {
+    
+    public int findMiss(int[] arr)
